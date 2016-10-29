cb93f54723d66998a1bd97d74006974dfa9ade68
alesbrelih <ales.brelih@gmail.com>
2016-10-29 21:21:00 +0200

    i dno wtf

--- server/server.js
+++ server/server.js
@@ -4,14 +4,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var shoppingItem = require("./models/item.model");
 var path = require("path");
 
 
 ////////////////////////////////
 // Router files
 ///////////////////////////////
-var itemsRoute = require("./routes/route-items")
+var itemsRoute = require("./routes/route-items");
 var indexRoute = require("./routes/route-index");
 
 // new express application
@@ -22,15 +21,15 @@ var app = express();
 /////////////////////////////////
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //USAGE OF CORS - CROSS DOMAIN RESPONSE SERVICE
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 ///////////////////////////////////
@@ -46,7 +45,7 @@ app.engine("html",require("ejs").renderFile);
 
 
 // Connect to mongoDb using mongoose ORM
-mongoose.connect('mongodb://localhost/shopping_list');
+mongoose.connect("mongodb://localhost/shopping_list");
 
 app.use("/",indexRoute);
 app.use("/api/items",itemsRoute);
