168787c3066c5de0dc96570049f598a3b3440ad8
ales.brelih@gmail.com <ales.brelih@gmail.com>
2018-12-27 20:25:29 +0100

    added

--- server/client/app/app.js
+++ server/client/app/app.js
@@ -8,5 +8,6 @@
     angular.module("shoppingListApp", dependencies);
 
 //TODO: add toasts for messages/warnings instead of modal
+//TODO: create filter for profile object on header
 })(window.angular);
 
